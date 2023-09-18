92adb68f5777e04388c1e7621434262390c1372a
Sue Ceklosky <cekloskys@chc.edu>
2023-09-18 13:55:32 -0400

    BinarySearch

---
+++ main.py
@@ -0,0 +1,25 @@
+from binarysearch import * 
+
+def main(): 
+    # Set up local variables to store the list, first, size, and target
+    a = []
+    first = 0
+    size = 0 
+    target = 0 
+
+    # Prompt the user for the elements adn inout into the list
+    a = list(map(int,input("Enter ten numbers seperated by space: ").split()))
+
+    # Prompt the user for the index at which the search will begin 
+    first = int(input("Enter the index at which the search will begin: "))
+
+    # Prompt the user for the target
+    target = int(input("Enter the target value to search for: "))
+
+    # Call search and display its return
+    print("Target found at index....",search(a,first,target))
+
+
+
+if __name__ == "__main__":
+    main()
